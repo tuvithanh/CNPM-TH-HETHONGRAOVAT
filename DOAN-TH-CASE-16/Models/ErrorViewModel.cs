c67eda9b3072725d1f29ec9a12184ff93bfdcf92
hnahtIv <61571748+tuvithanh@users.noreply.github.com>
2025-09-24 07:13:34 +0700

    initial project
    tao project

---
+++ DOAN-TH-CASE-16/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DOAN_TH_CASE_16.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
