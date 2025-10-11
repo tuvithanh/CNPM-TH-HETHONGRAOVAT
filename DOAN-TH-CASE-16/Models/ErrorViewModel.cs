12b6651961979003dc7f13af1b08a2e435c28ad7
hnahtIv <61571748+tuvithanh@users.noreply.github.com>
2025-10-11 17:10:51 +0700

    update file2

--- DOAN-TH-CASE-16/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DOAN_TH_CASE_16.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
